b8bb1fa02b4024e8c577ec918498d12958fb231b
Rasmus Møller Kristensen <eaa24ramk@students.eaaa.dk>
2025-04-24 09:56:41 +0200

    Local storage implementeret

--- GenbrugsMarked/WebApp/Program.cs
+++ GenbrugsMarked/WebApp/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebApp;
 using WebApp.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredLocalStorage(); // flyt måske
 builder.Services.AddScoped<UserService>();
 
 builder.Services.AddScoped(sp =>
